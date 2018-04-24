1d1851ac7d2053a3a16bb36c436c4427b0cb99c0
Kőszegi Richárd <richard.koszegi95@gmail.com>
2018-04-24 10:01:01 +0200

    Initial commit

---
+++ src/main/java/hu/rkoszegi/balancer/BalancerApplication.java
@@ -0,0 +1,12 @@
+package hu.rkoszegi.balancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalancerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalancerApplication.class, args);
+    }
+}
---
+++ src/test/java/hu/rkoszegi/balancer/BalancerApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.rkoszegi.balancer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BalancerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
