fa04dea95bd8f4c2ff1607f9cb488c335a348929
Richárd Kőszegi <richard.koszegi@webvalto.hu>
2018-10-22 21:32:37 +0200

    Add update task to ProjectDetails view

--- src/main/angular-frontend/src/app/components/project-details/new-task-modal/new-task-modal.component.ts
+++ src/main/angular-frontend/src/app/components/project-details/new-task-modal/new-task-modal.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Priority} from "../../../model/Priority";
 import {ProjectDetailsService} from "../../../services/ProjectDetailsService";
+import {Task} from "../../../model/Task";
+import {AlertService} from "../../../services/AlertService";
 
 @Component({
   selector: 'app-new-task-modal',
@@ -14,28 +16,62 @@ export class NewTaskModalComponent implements OnInit {
 
   priorities: string[];
 
-  constructor(private projectDetailsService: ProjectDetailsService) {
-  }
+  editMode: boolean;
+
+  @Input()
+  editedTask: Task;
+
+  constructor(
+    private alertService: AlertService,
+    private projectDetailsService: ProjectDetailsService
+  ) {}
 
   initForm(): void {
-    this.newTaskForm = new FormGroup({
-      'name': new FormControl('', Validators.required),
-      'priority': new FormControl(''),
-      'plannedDate': new FormControl(''),
-      'description': new FormControl('')
-    });
+
+    if(this.editMode) {
+      this.newTaskForm = new FormGroup({
+        'name': new FormControl(this.editedTask.name, Validators.required),
+        'priority': new FormControl(this.editedTask.priority),
+        'plannedDate': new FormControl(this.editedTask.plannedDate),
+        'description': new FormControl(this.editedTask.description)
+      });
+    } else {
+      this.newTaskForm = new FormGroup({
+        'name': new FormControl('', Validators.required),
+        'priority': new FormControl('C'),
+        'plannedDate': new FormControl(''),
+        'description': new FormControl('')
+      });
+    }
   }
 
   ngOnInit() {
     //help from: https://stackoverflow.com/a/49417089
     //and https://stackoverflow.com/a/48610402
     this.priorities = Object.values(Priority).filter(priority => typeof priority == "string");
+
+    this.editMode = this.editedTask != null;
+
     this.initForm();
   }
 
-  onCreateTask() {
-    this.projectDetailsService.createTaskForProject(this.newTaskForm.value);
-    this.newTaskForm.reset();
+  onSubmit() {
+    if (this.editMode) {
+      this.updateEditedTaskFieldsFromForm();
+      this.projectDetailsService.updateTask(this.editedTask).subscribe(() => {
+        this.alertService.success('Task updated!');
+      });
+    } else {
+      this.projectDetailsService.createTaskForProject(this.newTaskForm.value);
+      this.newTaskForm.reset();
+    }
+  }
+
+  private updateEditedTaskFieldsFromForm() {
+    this.editedTask.name = this.newTaskForm.value['name'];
+    this.editedTask.priority = this.newTaskForm.value['priority'];
+    this.editedTask.plannedDate = this.newTaskForm.value['plannedDate'];
+    this.editedTask.description = this.newTaskForm.value['description'];
   }
 
   onCancelCreateTask(): void {
