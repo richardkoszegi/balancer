399bd80a5e331188422362d9c46915d98304c900
Richárd Kőszegi <richard.koszegi95@gmail.com>
2018-11-06 01:21:19 +0100

    Create first admin user if not exists at startup

---
+++ src/main/java/hu/rkoszegi/balancer/bootstrap/UserBootstrap.java
@@ -0,0 +1,35 @@
+package hu.rkoszegi.balancer.bootstrap;
+
+import hu.rkoszegi.balancer.model.User;
+import hu.rkoszegi.balancer.model.UserRole;
+import hu.rkoszegi.balancer.repositories.UserRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private static final String FIRST_ADMIN_USER_NAME = "FirstAdminUser";
+
+    private UserRepository userRepository;
+
+    public UserBootstrap(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initFirstAdmin();
+    }
+
+    private void initFirstAdmin() {
+        if(userRepository.findUserByUsername(FIRST_ADMIN_USER_NAME) == null) {
+            User user = new User();
+            user.setUsername(FIRST_ADMIN_USER_NAME);
+            user.setPassword("$2a$11$h5mzTndV8pUw9xbvwMyigOpVEUpV5iK66gz/rquD773P6CHSIgRxW");
+            user.setRole(UserRole.ROLE_ADMIN);
+            userRepository.save(user);
+        }
+    }
+}
